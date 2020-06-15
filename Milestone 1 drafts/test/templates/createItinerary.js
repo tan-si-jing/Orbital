2049594a856897c36ec7c8ed6035a8b1977e94e9
Vanessa <vanessateo2000@gmail.com>
2020-06-15 23:09:48 +0800

    message

---
+++ Milestone 1 drafts/test/templates/createItinerary.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("button");
+var d3 = document.getElementById("d3");
+function createItinerary() {
+    button.style.display="none";
+    d3.style.display="block";
+}
