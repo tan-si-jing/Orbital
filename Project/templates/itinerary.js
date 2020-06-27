26499d1c81c9c27d9a42562e64561c68a22bda15
tan-si-jing <tan-si-jing@users.noreply.github.com>
2020-06-28 03:12:49 +0800

    update template files

--- Project/templates/itinerary.js
+++ Project/templates/itinerary.js
@@ -37,18 +37,26 @@ $(document).ready(function () {
             var country = $('#country option:selected').val();
             var startDate = $('#startDate').val();
             var endDate = $('#endDate').val();
-            $.ajax({
-              type: 'POST',
-              data: {
-                name:itnName,
-                country:country,
-                start_date:startDate,
-                end_date:endDate,
-              },
-              url: "/newItn"
+            var data = {
+              name:itnName,
+              country:country,
+              start_date:startDate,
+              end_date:endDate,
+            };
+            console.log(data);
+            fetch('/newItn', {
+              method: "POST",
+              credentials: "include",
+              body: JSON.stringify(data),
+              headers: new Headers({
+                "content-type": "application/json"
+              })
             })
-            dialog.dialog("close");
-            return true;
+            .then(response => {
+              if (response.redirected) {
+                window.location.href = response.url;
+              }
+            });
         }
 
         dialog = $("#dialog-form").dialog({
